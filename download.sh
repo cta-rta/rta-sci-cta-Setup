7b70b42a31eacb9ca53f5c4c02eaa12d61a17e2c
Nicolò Parmiggiani <nicolo.parmiggiani@gmail.com>
2018-09-14 10:59:05 +0200

    removed RTAlib from BUILD1

--- download.sh
+++ download.sh
@@ -7,7 +7,6 @@ echo $2
 if [ "$2" != "-t" ] ; then
 	echo "USE TAGS"
 	#TAG_PIPEMANAGER=v1.0.2
-	#TAG_RTALIB=v2.0.0
   #TAG_GAMMAPIPECOMMON=v1.0.0
   #TAG_CTAGAMMAPIPECOMMON=v1.0.0
   #TAG_CTOOLSING=v1.0.0
@@ -21,16 +20,6 @@ if [ "$2" != "-t" ]; then
 	git checkout $TAG_PIPEMANAGER
 else
 	echo "Use master version"
-fi
-cd ..
-
-echo "--------------"
-git clone https://$1@github.com/rta-pipe/RTAlib.git
-cd RTAlib
-if [ "$2" != "-t" ]; then
-        git checkout $TAG_RTALIB
-else
-        echo "Use master version"
 fi
 cd ..
 
